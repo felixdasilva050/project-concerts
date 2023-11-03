260eb1c1bf4db70251c8fd3c698ba0342e83bd4b
Guilherme Silva <guilhermefelix.ofwork@gmail.com>
2023-11-03 19:50:22 -0300

    Create firebase.py

---
+++ app/firebase.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+URL_FIREBASE = 'https://concerts-estacio-default-rtdb.firebaseio.com/'
+
+##begin CRUD 
+def create(dados:dict):
+    request = requests.post(f'{URL_FIREBASE}/events/.json', data= json.dumps(dados))
+    if(request.status_code != 200):
+        print(request.text)
+
+def readAll():
+    request = requests.get(f'{URL_FIREBASE}/events/.json')
+    if(request.status_code != 200):
+        print((f'code error: {request} text: {request.text}'))
+    return request.json()
+
+def updateById(dados:dict, id:str):
+    request = requests.patch(f'{URL_FIREBASE}/events/{id}/.json', data= json.dumps(dados))
+    if(request.status_code != 200):
+        print((f'code error: {request} text: {request.text}'))
+
+def deleteById(id:str):
+    request = requests.patch(f'{URL_FIREBASE}/events/{id}/.json')
+    if(request.status_code != 200):
+        print((f'code error: {request} text: {request.text}'))
+
+##end CRUD
